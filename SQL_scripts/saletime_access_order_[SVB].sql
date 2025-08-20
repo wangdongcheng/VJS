f50523cd2ce3894c207a9457bb96476078b3a95d
wangdongcheng <wangdongcheng@gmail.com>
2025-08-20 10:28:58 +0200

    2

--- SQL_scripts/saletime_access_order_[SVB].sql
+++ SQL_scripts/saletime_access_order_[SVB].sql
@@ -1,5 +1,5 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
-USE Salestime_SVB
+USE Salestime_SVB;
 SELECT OL.Orderid as 'ST_OrdNo', OH_ORDER_NUMBER as 'AccOrdNo.', ol.id as ST_LineID, od.od_dimension3 as AccDim3, DefaultRep, CustomerCode, LocationCode, OD_LOCATN as 'Access WH', ol.StockCode, S.STKNAME,
     StockCondition, cast(TargetQtyFrom as decimal) as TargetQtyFrom, cast(TargetQtyTo as decimal) as TargetQtyTo, dl.DiscountType, cast(Value as decimal) as 'Percentage', cast(ol.TotalUnitQuantity as decimal) as 'ST_Qty', od.OD_QTYPRINTED as 'Acc_Qty_P', od.OD_QTYINVD as 'Acc_Qty', cast(ol.Price as decimal(10,2)) as 'ST_Price', OD.OD_UNITCST as 'Acc_Price',
     cast(ol.NetAmount as decimal(10,2)) as 'ST_NetAmt', OD.OD_NETT as 'Acc_NetAmt', ol.DiscountAmount as ST_Disc, OD_T_DISCVAL as Acc_Disc, OH_DISC_TOTAL_P as Acc_OverallDisc, ol.DateCreated, OH_DATE_PUTIN as 'Access Entry Date',
@@ -14,4 +14,4 @@ from Orders O
 where OH_ORDER_NUMBER = '48125'
 --where ol.Orderid = '1044784'
 --AND DefaultRep = ''
-order by StockCode
+order by StockCode;
