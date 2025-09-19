171672c1608c16efc8c2d0bdec13e16478af03ee
wangdongcheng <wangdongcheng@gmail.com>
2025-09-19 13:41:30 +0200

    1

---
+++ SQL_scripts/Crystal_Report/SQL query.sql
@@ -0,0 +1,178 @@
+SELECT
+    "ORD_HEADER"."OH_ACCOUNT",
+    "ORD_HEADER"."OH_ORDER_NUMBER",
+    "DOC_ORDER_HEADER"."DOH_DOC_DATE",
+    "ORD_DETAIL"."OD_STOCK_CODE",
+    "STK_STOCK"."STKNAME",
+    "DOC_ORDER_DETAIL"."DOD_QUANTITY",
+    "ORD_DETAIL"."OD_TABLECODE",
+    "STK_STOCK_2"."STK_SELL_NAME1",
+    "STK_STOCK_2"."STK_SELL_NAME2",
+    "STK_STOCK_2"."STK_SELL_NAME3",
+    "STK_STOCK_2"."STK_SELL_NAME4",
+    "STK_STOCK_2"."STK_SELL_NAME5",
+    "STK_STOCK_2"."STK_SELL_NAME6",
+    "STK_STOCK_2"."STK_SELL_NAME7",
+    "STK_STOCK_2"."STK_SELL_NAME8",
+    "STK_STOCK_2"."STK_SELL_NAME9",
+    "STK_STOCK_2"."STK_SELL_NAME10",
+    "DOC_ORDER_DETAIL"."DOD_LINEDISC_DOC",
+    "DOC_ORDER_DETAIL"."DOD_NET_DOC",
+    "ORD_HEADER"."OH_CURRENCYCODE",
+    "DOC_ORDER_HEADER"."DOH_VAT_NET2",
+    "DOC_ORDER_HEADER"."DOH_VAT_NET3",
+    "DOC_ORDER_HEADER"."DOH_VAT_NET4",
+    "DOC_ORDER_HEADER"."DOH_VAT_NET5",
+    "DOC_ORDER_HEADER"."DOH_DOC_DUEDATE",
+    "ORD_HEADER"."OH_DISC_TOTAL_P",
+    "ORD_DETAIL"."OD_VATCODE",
+    "DOC_ORDER_HEADER"."DOH_ORDER_LINK",
+    "DOC_ORDER_DETAIL"."DOD_VAT_DOC",
+    "DOC_ORDER_HEADER"."DOH_VAT_NET1",
+    "DOC_ORDER_HEADER"."DOH_VAT_VALUE1",
+    "DOC_ORDER_HEADER"."DOH_VAT_VALUE2",
+    "DOC_ORDER_HEADER"."DOH_VAT_VALUE3",
+    "DOC_ORDER_HEADER"."DOH_VAT_VALUE5",
+    "DOC_ORDER_HEADER"."DOH_VAT_VALUE4",
+    "DOC_ORDER_DETAIL"."DOD_TOTDISC_DOC",
+    "DOC_ORDER_HEADER"."DOH_DOC_NUMBER",
+    "SL_ACCOUNTS"."CUNAME",
+    "SYS_VATCONTROL"."VAT_RATE",
+    "SYS_VATCONTROL2"."VAT_RATE",
+    "SYS_VATCONTROL3"."VAT_RATE",
+    "SYS_VATCONTROL4"."VAT_RATE",
+    "SL_ACCOUNTS"."CU_VAT_REG_NO",
+    "SL_ADDRESSES"."AD_ADDRESS",
+    "SL_ADDRESSES"."AD_ADDRESS_USER1",
+    "SL_ADDRESSES"."AD_ADDRESS_USER2",
+    "SL_ADDRESSES"."AD_POSTCODE",
+    "SYS_DATAINFO"."HOME_CURR_SYMBL",
+    "DOC_ORDER_DETAIL"."DOD_VAT_BASE1",
+    "SL_ADDRESSES"."AD_ACCOUNTNAME",
+    "DOC_ORDER_DETAIL"."DOD_UNITCST_DOC",
+    "SYS_DATAINFO"."COMPANY_NAME",
+    "SYS_DATAINFO"."COMP_ADDRESS",
+    "SYS_DATAINFO"."COMP_ADDRESS_USER1",
+    "SYS_DATAINFO"."COMP_ADDRESS_USER2",
+    "SYS_DATAINFO"."COMP_POSTCODE",
+    "SYS_DATAINFO"."COMP_VATNUMBER",
+    "STK_STOCK_2"."STK_SELLPRICE10",
+    "ORD_HEADER"."OH_USER3",
+    "ORD_HEADER"."OH_USER1",
+    "SL_ADDRESSES2"."AD_ACCOUNTNAME",
+    "SL_ADDRESSES2"."AD_ADDRESS",
+    "SL_ADDRESSES2"."AD_ADDRESS_USER1",
+    "SL_ADDRESSES2"."AD_ADDRESS_USER2",
+    "SL_ADDRESSES2"."AD_POSTCODE",
+    "SL_ADDRESSES"."AD_PHONE",
+    "DOC_ORDER_HEADER"."DOH_DOC_TYPE",
+    "SL_ADDRESSES"."AD_MOBILE",
+    "SYS_DATAINFO2"."COMP_USERFIELD1",
+    "STK_LOCATION"."LOC_USERSORT2",
+    "STK_LOCATION2"."LOC_USERDATE1",
+    "SL_ACCOUNTS"."CU_TERMS",
+    "ORD_DETAIL"."OD_ENTRY_TYPE",
+    "ORD_DETAIL"."OD_DETAIL",
+    "STK_STOCK3"."STK_USRCHAR5",
+    "SYS_DATAINFO2"."STK_SOP_COPY_DESC",
+    "SYS_DATAINFO2"."COMP_USERFIELD2",
+    "SL_ACCOUNTS2"."CU_BANK_AC_NO",
+    "PTL_COPIES"."CC_COPIES",
+    "PTL_COPIES"."CC_DESCRIPTION",
+    "SL_ACCOUNTS2"."CU_USRFLAG3",
+    "SYS_DATAINFO"."COMP_REGNUMBER",
+    "SL_ACCOUNTS2"."CU_USRFLAG4",
+    "SL_ACCOUNTS"."CUUSER3",
+    "SL_ACCOUNTS"."CUCODE",
+    "STK_STOCK"."STK_SORT_KEY",
+    "ORD_DETAIL"."OD_DIMENSION5",
+    "STK_STOCK"."STK_EC_SUP_UNIT",
+    "STK_STOCK3"."STK_USRFLAG8",
+    "STK_STOCK"."STK_SORT_KEY1",
+    "DOC_ORDER_DETAIL"."DOD_MOVEMENT_DETAIL_TEXT",
+    "SYS_VATCONTROL"."VAT_CODE",
+    "SYS_VATCONTROL2"."VAT_CODE",
+    "SYS_VATCONTROL3"."VAT_CODE",
+    "SYS_VATCONTROL4"."VAT_CODE",
+    "ORD_HEADER"."OH_INTERNL_NOTE"
+FROM
+    (
+        (
+            (
+                (
+                    (
+                        (
+                            (
+                                (
+                                    (
+                                        (
+                                            (
+                                                (
+                                                    (
+                                                        (
+                                                            (
+                                                                (
+                                                                    (
+                                                                        (
+                                                                            "VJSCL"."dbo"."PTL_COPIES" "PTL_COPIES"
+                                                                            INNER JOIN "VJSCL"."dbo"."DOC_ORDER_DETAIL" "DOC_ORDER_DETAIL" ON "PTL_COPIES"."CC_COPIES" <> "DOC_ORDER_DETAIL"."DOD_ORDER_LINK"
+                                                                        )
+                                                                        INNER JOIN "VJSCL"."dbo"."DOC_ORDER_HEADER" "DOC_ORDER_HEADER" ON "DOC_ORDER_DETAIL"."DOD_HEADER_LINK" = "DOC_ORDER_HEADER"."DOH_PRIMARY"
+                                                                    )
+                                                                    INNER JOIN "VJSCL"."dbo"."ORD_DETAIL" "ORD_DETAIL" ON "DOC_ORDER_DETAIL"."DOD_ORDER_LINK" = "ORD_DETAIL"."OD_PRIMARY"
+                                                                )
+                                                                INNER JOIN "VJSCL"."dbo"."ORD_HEADER" "ORD_HEADER" ON "DOC_ORDER_HEADER"."DOH_ORDER_LINK" = "ORD_HEADER"."OH_PRIMARY"
+                                                            )
+                                                            INNER JOIN "VJSCL"."dbo"."SYS_DATAINFO" "SYS_DATAINFO" ON "DOC_ORDER_HEADER"."DOH_SYS_LINK" = "SYS_DATAINFO"."SYS_PRIMARY"
+                                                        )
+                                                        LEFT OUTER JOIN "VJSCL"."dbo"."SYS_VATCONTROL" "SYS_VATCONTROL" ON "DOC_ORDER_HEADER"."DOH_VAT_CODE1" = "SYS_VATCONTROL"."VAT_CODE"
+                                                    )
+                                                    LEFT OUTER JOIN "VJSCL"."dbo"."SYS_VATCONTROL" "SYS_VATCONTROL3" ON "DOC_ORDER_HEADER"."DOH_VAT_CODE3" = "SYS_VATCONTROL3"."VAT_CODE"
+                                                )
+                                                LEFT OUTER JOIN "VJSCL"."dbo"."SYS_VATCONTROL" "SYS_VATCONTROL4" ON "DOC_ORDER_HEADER"."DOH_VAT_CODE4" = "SYS_VATCONTROL4"."VAT_CODE"
+                                            )
+                                            LEFT OUTER JOIN "VJSCL"."dbo"."SYS_VATCONTROL" "SYS_VATCONTROL2" ON "DOC_ORDER_HEADER"."DOH_VAT_CODE2" = "SYS_VATCONTROL2"."VAT_CODE"
+                                        )
+                                        INNER JOIN "VJSCL"."dbo"."SYS_DATAINFO2" "SYS_DATAINFO2" ON "SYS_DATAINFO"."SYS_PRIMARY" = "SYS_DATAINFO2"."SYS_PRIMARY_2"
+                                    )
+                                    INNER JOIN "VJSCL"."dbo"."SL_ACCOUNTS" "SL_ACCOUNTS" ON "ORD_HEADER"."OH_ACCOUNT" = "SL_ACCOUNTS"."CUCODE"
+                                )
+                                INNER JOIN "VJSCL"."dbo"."SL_ADDRESSES" "SL_ADDRESSES2" ON (
+                                    "ORD_HEADER"."OH_ACCOUNT" = "SL_ADDRESSES2"."AD_ACC_CODE"
+                                ) AND
+                                (
+                                    "ORD_HEADER"."OH_DEL_ADD" = "SL_ADDRESSES2"."AD_CODE"
+                                )
+                            )
+                            INNER JOIN "VJSCL"."dbo"."SL_ADDRESSES" "SL_ADDRESSES" ON (
+                                "ORD_HEADER"."OH_ACCOUNT" = "SL_ADDRESSES"."AD_ACC_CODE"
+                            ) AND
+                            (
+                                "ORD_HEADER"."OH_INV_ADD" = "SL_ADDRESSES"."AD_CODE"
+                            )
+                        )
+                        INNER JOIN "VJSCL"."dbo"."SL_ACCOUNTS2" "SL_ACCOUNTS2" ON "SL_ACCOUNTS"."CUCODE" = "SL_ACCOUNTS2"."CUCODE2"
+                    )
+                    LEFT OUTER JOIN "VJSCL"."dbo"."STK_STOCK" "STK_STOCK" ON "ORD_DETAIL"."OD_STOCK_CODE" = "STK_STOCK"."STKCODE"
+                )
+                LEFT OUTER JOIN "VJSCL"."dbo"."STK_LOCATION" "STK_LOCATION" ON (
+                    "ORD_DETAIL"."OD_LOCATN" = "STK_LOCATION"."LOC_CODE"
+                ) AND
+                (
+                    "ORD_DETAIL"."OD_STOCK_CODE" = "STK_LOCATION"."LOC_STOCK_CODE"
+                )
+            )
+            LEFT OUTER JOIN "VJSCL"."dbo"."STK_LOCATION2" "STK_LOCATION2" ON (
+                "STK_LOCATION"."LOC_CODE" = "STK_LOCATION2"."LOC_CODE2"
+            ) AND
+            (
+                "STK_LOCATION"."LOC_STOCK_CODE" = "STK_LOCATION2"."LOC_STOCKCODE2"
+            )
+        )
+        LEFT OUTER JOIN "VJSCL"."dbo"."STK_STOCK_2" "STK_STOCK_2" ON "STK_STOCK"."STKCODE" = "STK_STOCK_2"."STKCODE2"
+    )
+    LEFT OUTER JOIN "VJSCL"."dbo"."STK_STOCK3" "STK_STOCK3" ON "STK_STOCK"."STKCODE" = "STK_STOCK3"."STKCODE3"
+ORDER BY
+    "PTL_COPIES"."CC_COPIES",
+    "DOC_ORDER_HEADER"."DOH_ORDER_LINK",
+    "STK_STOCK"."STK_SORT_KEY"
--- SQL_scripts/_playground.sql
+++ SQL_scripts/_playground.sql
@@ -1,59 +1,8 @@
-declare @DateFrom datetime = '1/1/2025',
-        @DateTo DATETIME = '8/31/2025';
-
-print @DateFrom;
-print CONCAT(year(@DateFrom),'-',month(@DateFrom),'-',day(@DateFrom));
-print @DateTo;
-PRINT CONCAT(year(@DateTo),'-',month(@DateTo),'-',day(@DateTo));
-
-SELECT
-	'Staff Feeding Scheme' AS 'Activity',
-    det_date,
-    MONTH(DET_DATE) AS [Month],
-    DET_HEADER_REF AS InvoiceNumber,
-    DET_ACCOUNT AS CustCode,
-	STK_SUPSTKCDE1 AS Code,
-    STKCODE AS VJStockCode,
-    STKNAME AS Description,
-    DET_QUANTITY AS Qty,
-	DET_UNIT_PRICE AS OriginalInvUnitPrice,
-	DET_UNIT_PRICE-DET_L_DISCOUNT-DET_T_DISCOUNT AS DiscountedInvPrice,
-	(DET_L_DISCOUNT/nullif((DET_QUANTITY * DET_UNIT_PRICE),0))*100 AS '% Discount',
-	DET_COSTPRICE/0.865 AS SupplierPricePerUnit
-FROM SL_PL_NL_DETAIL
-INNER JOIN STK_STOCK ON STKCODE = DET_STOCK_CODE
-INNER JOIN STK_STOCK_2 S2 ON DET_STOCK_CODE=S2.STKCODE2
-WHERE STK_SORT_KEY3 = '30 HILLS PET NUTRI.' AND DET_ORIGIN = 'SO' AND DET_TYPE = 'INV' 
-AND DET_DATE BETWEEN CONCAT(year(@DateFrom),'-',month(@DateFrom),'-',day(@DateFrom))
-AND CONCAT(year(@DateTo),'-',month(@DateTo),'-',day(@DateTo))
-	AND DET_ACCOUNT = '30VCS001'
-
-select top 1 CONCAT(year(@DateFrom),'-',month(@DateFrom),'-',day(@DateFrom),'and',year(@DateTo),'-',month(@DateTo),'-',day(@DateTo)) as 'Date Range';    
-
-select STKCODE as 'Stock Code',
-stkname as 'Stock Name',
-stk_p_weight as 'Duty' 
-from stk_stock 
-where stk_p_weight != 0 and STKCODE like '30euk_%' order by stkcode;
+select top 100 STK_SELL_NAME1,* from STK_STOCK_2
+where STK_SELL_NAME1 = 'm-pack';
 
 
-select top 100 cu_usrflag3,* from sl_accounts2 where cu_usrflag3 = 0;
-
-
-select distinct cuuser1 from sl_accounts order by cuuser1;
-
-
-select top 100 
-DOH_VAT_CODE1,doh_vat_net1,doh_vat_value1,
-DOH_VAT_CODE2,doh_vat_net2,doh_vat_value2,
-DOH_VAT_CODE3,doh_vat_net3,doh_vat_value3,
-DOH_VAT_CODE4,doh_vat_net4,doh_vat_value4,
-DOH_VAT_CODE5,doh_vat_net5,doh_vat_value5,
-* from doc_order_header 
-where 
-DOH_DOC_NUMBER = '392962'
---DOH_VAT_CODE3 != '';
-
-
-select * from SYS_VATCONTROL
-
+SELECT CUCODE, CUNAME, CU_INV_LAYOUT_LINK, D.SDL_DOC_NAME
+FROM SL_ACCOUNTS A
+INNER JOIN [SYS_DOCUMENT_LAYOUTS] D ON A.CU_INV_LAYOUT_LINK=D.SDL_PRIMARY
+WHERE CU_DO_NOT_USE = 0 AND CU_INV_LAYOUT_LINK IS NOT NULL AND CU_INV_LAYOUT_LINK != 0 AND CU_INV_LAYOUT_LINK != 1
