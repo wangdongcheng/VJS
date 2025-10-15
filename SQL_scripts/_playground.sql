1fc916e6cd2cef0b7bcd9cca3021791c4cc064a8
wangdongcheng <wangdongcheng@gmail.com>
2025-10-15 16:40:35 +0200

    1

--- SQL_scripts/_playground.sql
+++ SQL_scripts/_playground.sql
@@ -20,15 +20,19 @@ select top 100 stk_sort_key, * from vjscl.dbo.STK_STOCK
 where stk_sort_key like '%actiph%';
 where STKCODE = '30JEY_2006512UX';
 
-select pl2.sucode2,pl.suname, pl2.su_usrchar1, pl2.su_usrchar2, pl2.su_usrchar5,* 
-from vjsp.dbo.pl_accounts2 pl2 INNER join vjsp.dbo.pl_accounts pl on pl2.sucode2 = pl.SUCODE
-where
-pl.suname in (
-    'AIRSWIFT COURIERS LTD');
+select pl2.sucode2,pl.suname, pl.SU_ADDRESS_USER1,pl.SUPOSTCODE,pl2.su_usrchar1, pl2.su_usrchar2, pl2.su_usrchar5,* 
+from vjscl.dbo.pl_accounts pl left outer join vjscl.dbo.pl_accounts2 pl2 on pl.SUCODE = pl2.sucode2
+order by 1;
 
 
+select stk.STKCODE,stk.stk_physical,stk.STK_QTYPRINTED,stk.STK_RESERVE_OUT,*
+from vjscl.dbo.stk_stock stk
+where 
+stk.stkcode IN (
+ '30FIO_56904570T',
+ '308in1_02281ux'
+)
+-- stk.STK_RESERVE_OUT > 0
+-- stk.STK_WO_UNALLOC_QUANTITY > 0
+;
 
-
--- su_usrchar1 != '' OR
--- SU_USRCHAR2 != '' OR
--- SU_USRCHAR5 != '';
