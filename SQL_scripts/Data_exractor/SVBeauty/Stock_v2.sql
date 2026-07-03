330646f84c1d036e1e1d24fff390af1d09896520
wangdongcheng <wangdongcheng@gmail.com>
2026-07-03 14:53:50 +0200

    9

--- SQL_scripts/Data_exractor/SVBeauty/Stock_v2.sql
+++ SQL_scripts/Data_exractor/SVBeauty/Stock_v2.sql
@@ -14,11 +14,12 @@ SELECT
 	s.stk_min_qty AS 'Main - Minimum',
 	s.stk_max_qty AS 'Main - Maximum',
 	s.stk_bin_number AS 'Main - Bin Number',
-	s.stk_sort_key AS 'Sort Key - Brand',
-	s.stk_sort_key1 AS 'Sort Key - Sub Category',
+	--- Sort Key tab
+	s.stk_sort_key AS 'Sort Key - Main Category',
+	s.stk_sort_key1 AS 'Sort Key - Brand Name',
 	s.stk_sort_key2 AS 'Sort Key - Type',
 	s.stk_sort_key3 AS 'Sort Key - Supplier',
-	--- Info dialog ---
+	--- Info dialog
 	CAST(s.stk_date_putin AS DATE) AS 'Info - Created On',
 	CAST(s.stk_date_edited AS DATE) AS 'Info - Last Changed',
 	s.stk_oldcode AS 'Info - Superseded by Code',
@@ -26,15 +27,15 @@ SELECT
 	s.stk_reverse_chrge_vat AS 'Info - Reverse Charge VAT applies',
 	s.stk_do_not_use AS 'Info - Inactive',
 	s.stk_www_publish AS 'Info - Publish on World Wide Web',
-	--- EC Info dialog ---
-	s.stk_ec_kilos AS 'EC Info - Exp Days Alarm',
+	--- EC Info dialog
+	s.stk_ec_kilos AS 'EC Info - Net Mass kg',
 	s.stk_ec_origin AS 'EC Info - Country of Origin',
 	s.stk_ec_com_code AS 'EC Info - Commodity Code',
-	s.stk_ec_sup_unit AS 'EC Info - Units per Case',
-	s.stk_ec_sup_unit_type AS 'EC Info - Type (of Units per Case)',
+	s.stk_ec_sup_unit AS 'EC Info - Supplementary Units',
+	s.stk_ec_sup_unit_type AS 'EC Info - Type (of Supplementary Units)',
 	s.stk_service AS 'EC Info - Service',
 	s.stk_place_of_supply_vat AS 'EC Info - Apply Place of Supply VAT Rules',
-	--- Profit dialog ---
+	--- Profit dialog
 	s.STK_SALEVALUE AS 'Profit - Sale Value - Total',
 	s.STK_COSTVALUE AS 'Profit - Cost of Sales - Total',
 	s.STK_SALEVALUE - s.STK_COSTVALUE AS 'Profit - Profit - Total',
@@ -47,16 +48,18 @@ SELECT
 	s.STK_COSTVAL_YTD AS 'Profit - Cost of Sales - Year',
 	s.STK_SALEVAL_YTD - s.STK_COSTVAL_YTD AS 'Profit - Profit - Year',
 	(s.STK_SALEVAL_YTD - s.STK_COSTVAL_YTD) / NULLIF(s.STK_SALEVAL_YTD, 0) * 100 AS 'Profit - Margin % - Year',
-	--- Notes tab ---
+	--- Notes tab
 	s.stk_notes AS 'Notes - Notes',
-	--- Picture tab ---
-	--- Weights tab ---
-	s.stk_p_weight AS 'Weights - Weight Duty',
-	s.stk_s_weight AS 'Weights - Weight Sell',
-	s.stk_p_wght_name AS 'Weights - Description Duty',
+	--- Picture tab
+	--- Weights tab
+	s.stk_p_weight AS 'Weights - Excise EUR',
+	s.stk_s_weight AS 'Weights - Excise Sell',
+	s.stk_p_wght_name AS 'Weights - Description EUR',
 	s.stk_s_wght_name AS 'Weights - Description Sell',
 	S.STK_BARCODE AS 'Weights - Barcode',
-	--- Custom tab ---
+	--- Custom tab
+	s3.STK_USRCHAR10 as 'Custom - Stk Brand Class',
+	--continue from here, 20260703_145308 done mark
 	s3.stk_usrflag3 AS 'Custom - Delisted',
 	s3.stk_usrflag1 AS 'Custom - Do Not Load',
 	s3.stk_usrchar2 AS 'Custom - Comp. Division',
@@ -87,7 +90,7 @@ SELECT
 	s3.STK_USRCHAR17 AS 'Custom - Champion Categ',
 	s3.STK_USRNUM9 AS 'Custom - Weight / Pallet',
 	s3.STK_USRCHAR18 AS 'Custom - Categ Manager',
-	--- Sell price table ---
+	--- Sell price table
 	s2.STK_SANALYSIS1 AS 'Selling Price 1 - Analysis',
 	s2.STK_SANALYSIS2 AS 'Selling Price 2 - Analysis',
 	s2.STK_SANALYSIS3 AS 'Selling Price 3 - Analysis',
@@ -108,35 +111,22 @@ SELECT
 	s2.STK_SELLPRICE8 'Selling Price 8 - AGENT',
 	s2.STK_SELLPRICE9 'Selling Price 9 - SUB_A RETAIL',
 	s2.STK_SELLPRICE10 'Selling Price 10 - CONSUMER'
-
 FROM
 	STK_STOCK S
 	INNER JOIN STK_STOCK3 S3 ON S.STKCODE = S3.STKCODE3
 	INNER JOIN STK_STOCK_2 S2 ON S.STKCODE = S2.STKCODE2
-	-- INNER JOIN SL_ANALYSIS SL ON S2.STK_SANALYSIS1 = SL.SACODE
-	-- INNER JOIN SYS_VATCONTROL V ON SL.SAVATCODE = V.VAT_CODE
-	-- INNER JOIN (
-	-- 	SELECT DISTINCT
-	-- 		LOC_STOCKCODE2,
-	-- 		LOC_USERFLAG1,
-	-- 		LOC_USERFLAG2
-	-- 	FROM
-	-- 		STK_LOCATION2
-	-- ) L ON S.STKCODE = L.LOC_STOCKCODE2
--- WHERE
-	-- s.STK_DO_NOT_USE = 0 
-	-- AND
-	-- S3.STK_USRFLAG3 = 0
-	-- AND
-	-- STK_SORT_KEY IN (
-	-- 	'30 NIVEA',
-	-- 	'30 ELASTOPLAST',
-	-- 	'30 DEMAKUP',
-	-- 	'30 FIOCCO',
-	-- 	'30 NUVENIA',
-	-- 	'30 TEMPO'
-	-- )
-	-- and stkcode = '30ffk_15086'
+WHERE
+	s.stkcode = '20EYL6001102'
 ORDER BY
-	stk_sort_key,
 	stkcode;
+
+RETURN;
+
+SELECT
+	*
+FROM
+	svbeautytest.dbo.STK_STOCK S
+	INNER JOIN svbeautytest.dbo.STK_STOCK3 S3 ON S.STKCODE = S3.STKCODE3
+	INNER JOIN svbeautytest.dbo.STK_STOCK_2 S2 ON S.STKCODE = S2.STKCODE2
+WHERE
+	s.stkcode = '20EYL6001102';
